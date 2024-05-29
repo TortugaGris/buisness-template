4ad28a810c5e736a27c42df5766c4cd01a09cee4
Javier Zavala <javierzavalatorres@gmail.com>
2024-05-29 16:52:30 -0400

    add service card animations

--- src/components/service-card.component.js
+++ src/components/service-card.component.js
@@ -3,15 +3,15 @@ export class ServiceCard extends HTMLElement {
         return `
             <article 
                 id='serviceCard' 
-                class="p-4 shadow-sm h-100 d-flex align-items-start flex-column"
+                class="p-4 shadow-sm h-100 d-flex align-items-start flex-column service-card"
                 data-mdb-animation-init
                 data-mdb-animation="fade-in-down"
                 data-mdb-animation-start="onScroll"
             >
-                <div class="border p-2 bg-primary bg-opacity-75 d-inline-block rounded-3">
+                <div id="bgIcon" class="border p-2 bg-primary bg-opacity-75 d-inline-block rounded-3">
                     <img width="50px" src="${this.attributes.img.value}" alt="">
                 </div>
-                <h3 class="mt-4 mb-2 h4 fw-semibold">${this.attributes.title.value}</h3>
+                <h3 class="mt-100px mb-2 h4 fw-semibold">${this.attributes.title.value}</h3>
                 <p class="text-muted">${this.attributes.body.value.trim().slice(0,200).trim()}...</p>
                 <div class="mt-auto align-self-end">
                     <i class="fa fa-arrow-right fs-4"></i>
